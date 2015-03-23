bff3e740093816b414ae0c134c3c99db4d646c4c
Hayley Swimelar <hswimelar@gmail.com>
2015-03-23 16:36:01 -0700

    add my_each method

--- enumerable.rb
+++ enumerable.rb
@@ -1,4 +1,13 @@
 module Enumerable
   def my_each
+    i = 0
+    self_len = self.length
+
+    while i < self_len
+      yield self[i]
+      i += 1
+    end
   end
 end
+
+[1, 2, 3, 4].my_each { |n| print n }
