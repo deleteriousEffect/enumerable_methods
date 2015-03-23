7e861a6a0421bd39a8acb9f75e31382b750161ec
Hayley Swimelar <hswimelar@gmail.com>
2015-03-23 16:44:03 -0700

    add my_each_with_index

--- enumerable.rb
+++ enumerable.rb
@@ -8,6 +8,18 @@ module Enumerable
       i += 1
     end
   end
+
+  def my_each_with_index
+    i = 0
+    self_len = self.length
+
+    while i < self_len
+      yield self[i], i
+      i += 1
+    end
+  end
 end
 
 [1, 2, 3, 4].my_each { |n| print n }
+
+[1, 2, 3, 4].my_each_with_index { |e, i| print "this is element #{e}, this is its index #{i} \n" }
