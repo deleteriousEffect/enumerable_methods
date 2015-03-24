40156c3b8b605efc3fa07f17d5705d1502cc00f2
Hayley Swimelar <hswimelar@gmail.com>
2015-03-23 17:03:14 -0700

    add my_all method

--- enumerable.rb
+++ enumerable.rb
@@ -25,10 +25,21 @@ module Enumerable
     end
     selected_elements
   end
+
+  def my_all
+    self.my_each do |element|
+      if (yield element) == false
+        return false
+      end
+      return true
+  end
 end
 
 [1, 2, 3, 4].my_each { |n| print n }
 
 [1, 2, 3, 4].my_each_with_index { |e, i| print "this is element #{e}, this is its index #{i} \n" }
 
-print [13, 12, 4, 5].my_select { |e| e % 2 == 0 }
+print [13, 12, 4, 5].my_select { |e| e.even? }
+
+[2, 4, 6, 8].my_all { |e| e.even? }
+[3, 4, 6, 8].my_all { |e| e.even? }
